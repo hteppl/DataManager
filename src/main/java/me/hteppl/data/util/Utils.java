d53b7d6d477e2699e73ff1e3e74ec760b8eaf31c
IWareQ <dimonpo96@mail.ru>
2022-08-05 20:51:38 +0400

    Fixed all exceptions

--- src/main/java/me/hteppl/data/util/Utils.java
+++ src/main/java/me/hteppl/data/util/Utils.java
@@ -3,6 +3,10 @@ package me.hteppl.data.util;
 import me.hteppl.data.DataManager;
 import org.sql2o.Sql2o;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Utils {
 
     public static Sql2o createSql2oForMySQL(String host, int port, String database, String user, String password) {
@@ -19,6 +23,12 @@ public class Utils {
     public static Sql2o createSql2oForSQLite(String folder, String database) {
         try {
             Class.forName("org.sqlite.JDBC");
+
+            try {
+                Files.createDirectories(Paths.get(folder));
+            } catch (IOException exception) {
+                throw new RuntimeException(exception);
+            }
 
             return new Sql2o("jdbc:sqlite:" + folder + "/" + database + ".db", null, null);
         } catch (ClassNotFoundException exception) {
