7d3bd4f554ea8ca5dcdeec5e633e969b71ace627
IWareQ <dimonpo96@mail.ru>
2022-09-30 16:07:19 +0400

    Fix random start exception

--- src/main/java/me/hteppl/data/database/MySQLDatabase.java
+++ src/main/java/me/hteppl/data/database/MySQLDatabase.java
@@ -11,14 +11,18 @@ public class MySQLDatabase extends Database {
     }
 
     public MySQLDatabase(String host, int port, String database, String user, String password) {
-        super(new Sql2o("jdbc:mysql://" + host + ":" + port + "/" + database + "?" + DataManager.getMysqlProperties(), user, password));
+        super(MySQLDatabase.createSql2o(host, port, database, user, password));
 
+        this.executeScheme("CREATE DATABASE IF NOT EXISTS " + database);
+    }
+
+    private static Sql2o createSql2o(String host, int port, String database, String user, String password) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException exception) {
             throw new RuntimeException(exception);
         }
 
-        this.executeScheme("CREATE DATABASE IF NOT EXISTS " + database);
+        return new Sql2o("jdbc:mysql://" + host + ":" + port + "/" + database + "?" + DataManager.getMysqlProperties(), user, password);
     }
 }
